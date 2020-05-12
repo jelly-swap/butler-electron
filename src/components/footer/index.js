97f23d775867ae9bd5c4bb6c088490e0a59ebc86
kraykov <raykov@weichain.tech>
2020-05-12 17:40:36 +0300

    Fix react router to work with electron. TODO: terminal to home does not work.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,34 +10,40 @@ import CoinImage from '../../css/background-coins/Coins-button.svg';
 import './style.scss';
 
 const statusText = {
-  '/': 'Start',
-  '/terminal': 'Stop',
+  '#/': 'Start',
+  '#/terminal': 'Stop',
 };
 
 const Footer = () => {
   const history = useHistory();
+  const location = history.location.hash;
+
+  console.log(location);
+  const buttonHandler = {
+    '#/': () => {
+      new Emitter().emitAll('startButler');
+    },
+    '#/terminal': () => {
+      const { ipcRenderer } = window.require('electron');
+      ipcRenderer.send('stop-butler');
+
+      ipcRenderer.on('butlerHasBeenKilled', (message, pathname) => {
+        history.push(pathname);
+      });
+    },
+  };
 
   return (
     <div className='footer-wrapper'>
       <Button
         btnText={
           <>
-            <span>{statusText[history.location.pathname]}</span>
+            <span>{statusText[location]}</span>
             <img src={CoinImage} alt='coin' />
           </>
         }
         onClick={() => {
-          const { ipcRenderer } = window.require('electron');
-
-          if (history.location.pathname === '/') {
-            new Emitter().emitAll('startButler');
-          } else {
-            ipcRenderer.send('stop-butler');
-
-            ipcRenderer.on('butlerHasBeenKilled', (message, pathname) => {
-              history.push(pathname);
-            });
-          }
+          buttonHandler[location]();
         }}
       />
     </div>
