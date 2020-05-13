567fee939de11a2d71196f0e1104ad15bf5fb1f1
kraykov <raykov@weichain.tech>
2020-05-13 13:49:59 +0300

    Butler updates.

--- src/butler/src/index.js
+++ src/butler/src/index.js
@@ -1,5 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const run_1 = require("./run");
-run_1.run();
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const run_1 = require('./run');
+const config = process.argv[2];
+// Electron fork
+if (config) {
+  run_1.run(JSON.parse(config));
+} else {
+  run_1.run();
+}
 //# sourceMappingURL=index.js.map
