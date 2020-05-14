149109c601ea2b0355ba64f6ea6a90a17af69a6c
Andon Mitev <mitevandon94@gmail.com>
2020-05-14 09:33:42 +0300

    Remove cl from butler

--- src/butler/src/index.js
+++ src/butler/src/index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 const run_1 = require('./run');
 const config = process.argv[2];
-console.log('check config', process.argv);
 // Electron fork
 if (config) {
   run_1.run(JSON.parse(config));
