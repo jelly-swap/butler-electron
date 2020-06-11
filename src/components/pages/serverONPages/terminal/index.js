89d9a5d21feb2e6119032ab0ba212191edd3550e
kraikov <raykov@weichain.tech>
2020-06-11 12:11:30 +0300

    Add log filter.

--- src/components/pages/serverONPages/terminal/index.js
+++ src/components/pages/serverONPages/terminal/index.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Terminal from 'terminal-in-react';
 
 import './style.scss';
+
+import { CheckboxRadio, useCheckboxes } from '../../../common/Checkbox';
 
 // 📕: error message
 // 📙: warning message
@@ -12,14 +14,28 @@ import './style.scss';
 
 const MESSAGE_COLORS = {
   ERROR: '📕',
-  WARNING: '📙',
+  WARN: '📙',
   INFO: '📗',
   ACTION: '📘',
   CANCELED: '📓',
   DEFAULT: '📔',
 };
 
+const checkboxesList = ['Info', 'Error', 'Warning'];
+
+const getDefaultCheckboxes = () =>
+  checkboxesList.map(checkbox => ({
+    name: checkbox,
+    checked: false,
+  }));
+
 const JellyTerminal = ({ terminalData }) => {
+  const [checkboxes, setCheckboxes] = useState(getDefaultCheckboxes());
+
+  useCheckboxes(checkboxes, setCheckboxes);
+
+  console.log(checkboxes);
+
   useEffect(() => {
     terminalData.forEach(log => {
       const { now, info } = log;
@@ -30,23 +46,43 @@ const JellyTerminal = ({ terminalData }) => {
 
       const color = idxOfDblDots !== -1 ? info.split(':')[0] : 'DEFAULT';
 
-      console.log(`${MESSAGE_COLORS[color]} ${now} ${info.replace(/INFO|ERROR/, '')}`);
+      console.log(`${MESSAGE_COLORS[color]} ${now} ${info.replace(/INFO|ERROR|WARN/, '')}`);
     });
   }, [terminalData]);
 
   return (
-    <div className='terminal-wrapper'>
-      <Terminal
-        allowTabs={false}
-        hideTopBar={true}
-        watchConsoleLogging={true}
-        color='green'
-        backgroundColor='#e4e4e4'
-        barColor='#e4e4e4'
-        style={{ fontWeight: 'bold', fontSize: '1em', height: '500px' }}
-      />
-    </div>
+    <>
+      {CheckboxRadio(checkboxes)}
+
+      <div className='terminal-wrapper'>
+        <Terminal
+          allowTabs={false}
+          hideTopBar={true}
+          watchConsoleLogging={true}
+          color='green'
+          backgroundColor='#e4e4e4'
+          barColor='#e4e4e4'
+          style={{ fontWeight: 'bold', fontSize: '1em', height: '500px' }}
+        />
+      </div>
+    </>
   );
 };
+
+// const labelToColor = {
+//   Info: 'INFO',
+//   Error: 'ERROR',
+//   Warning: 'WARN',
+// };
+
+// const getFilter = checked => {
+//   checked.reduce((c, p) => {
+//     if (c.checked) {
+//       p[labelToColor[c.name]] = true;
+//     }
+
+//     return p;
+//   }, {});
+// };
 
 export default JellyTerminal;
