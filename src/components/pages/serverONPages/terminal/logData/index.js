cdb8f6a983958057cd59eff8a6086b82a8779960
Andon Mitev <mitevandon94@gmail.com>
2020-07-28 09:19:07 +0300

    Dont push scroll to bottom if scroll is not on the bottom

--- src/components/pages/serverONPages/terminal/logData/index.js
+++ src/components/pages/serverONPages/terminal/logData/index.js
@@ -4,15 +4,28 @@ import Log from './log';
 
 export default ({ terminalData, selectedLogFilters }) => {
   const logsWrapperRef = useRef();
+  const isScrollAtBottomRef = useRef(true);
 
   useEffect(() => {
     if (!logsWrapperRef.current) return;
 
+    if (!isScrollAtBottomRef.current) return;
+
     logsWrapperRef.current.scrollTop = logsWrapperRef.current.scrollHeight;
   }, [terminalData]);
 
+  const handleOnScroll = event => {
+    event.persist();
+
+    const {
+      target: { scrollTop, clientHeight, scrollHeight },
+    } = event;
+
+    isScrollAtBottomRef.current = scrollTop + clientHeight === scrollHeight;
+  };
+
   return (
-    <div ref={logsWrapperRef} className='logs-wrapper'>
+    <div ref={logsWrapperRef} onScroll={handleOnScroll} className='logs-wrapper'>
       {terminalData.map(log => (
         <Log key={log.id} log={log} selectedLogFilters={selectedLogFilters} />
       ))}
