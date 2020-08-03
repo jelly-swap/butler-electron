41b71f2d8c993a11f5aa54301aa9190cee3edf61
kraikov <raykov@weichain.tech>
2020-08-03 10:19:43 +0300

    Update Butler's packages.

--- src/components/pages/serverONPages/index.js
+++ src/components/pages/serverONPages/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Route } from 'react-router-dom';
+
+import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
 
 import Terminal from './terminal';
 import BalanceOf from './balanceOf';
 
+import { useChannel } from '../../../hooks/useChannel';
+
+const ALLOWED_MESSAGES = ['DATA', 'ERROR'];
+const MAX_LOGS = 9999;
+const LOGS_TO_REMOVE = 1000;
+
+const getCurrentDate = () => moment().format('MMM Do YYYY h:mm:s A');
+const getMessageType = message => message?.indexOf(':') !== -1 && message?.split(':')[0];
+
 const ServerONPages = () => {
+  const [terminalData, setTerminalData] = useState([
+    { now: getCurrentDate(), info: ': Loading...', id: uuidv4(), msgType: 'DATA' },
+  ]);
+
+  const { data } = useChannel('data');
+
+  useEffect(() => {
+    const messageType = getMessageType(data);
+
+    if (ALLOWED_MESSAGES.includes(messageType)) {
+      const now = getCurrentDate();
+
+      const log = { now, info: data, msgType: messageType, id: uuidv4() };
+
+      setTerminalData(terminalData => [...terminalData, log]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (terminalData.length === MAX_LOGS) {
+      const dataRef = [...terminalData];
+
+      dataRef.splice(0, LOGS_TO_REMOVE);
+
+      setTerminalData(dataRef);
+    }
+  }, [terminalData]);
+
   return (
     <>
-      <Route exact path='/terminal' component={() => <Terminal />} />
+      <Route exact path='/terminal' component={() => <Terminal terminalData={terminalData} />} />
       <Route exact path='/balanceOf' component={() => <BalanceOf />} />
     </>
   );
