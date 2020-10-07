17f50438b0fa6405150954f1f003a3da568c17b6
kraikov <raykov@weichain.tech>
2020-10-07 12:16:45 +0300

    Fix typo.

--- public/butler/config.js
+++ public/butler/config.js
@@ -30,7 +30,7 @@ exports.default = {
     PRICE: {
         COINS: ['ETH', 'BTC', 'AE', 'DAI', 'WBTC', 'TBTC', 'USDC', 'USDT', 'ONE', 'MATIC', 'AVAX', 'BNB'],
     },
-    DUPLICATE_PRICE: { DAI: 'USDC', WBTC: 'BTC', TBTC: 'TBTC' },
+    DUPLICATE_PRICE: { DAI: 'USDC', WBTC: 'BTC', TBTC: 'BTC' },
     // ================== Binance configuration ==================
     BINANCE: {
         PAIRS: {
